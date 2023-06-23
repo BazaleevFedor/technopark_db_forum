b027ee28857802f73e151154afdfe8be0708cf42
Fedor <45175906+BazaleevFedor@users.noreply.github.com>
2023-06-23 23:40:40 +0300

    Update repo.go

--- internal/service/repo.go
+++ internal/service/repo.go
@@ -1,6 +1,6 @@
 package service
 
-import "github.com/Natali-Skv/technopark_db_forum/internal/models"
+import "github.com/BazaleevFedor/technopark_db_forum/internal/models"
 
 type Repo interface {
 	Status() (*models.Status, error)
